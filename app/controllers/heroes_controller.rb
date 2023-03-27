79dd738e0abb71106b916d1a3f85a9236e843941
sainamercy <sainamercy@gmail.com>
2023-03-27 21:45:06 +0300

    heroes error fix

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,5 +1,5 @@
 class HeroesController < ApplicationController
-    ActiveRecord::RecordNotFound, with: :not_found
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
    
     def index
         heroes = Heroe.all
